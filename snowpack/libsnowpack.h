d31499bf9f9d38de0987b5d21897087aad01c57b
Mathias Bavay <bavay@slf.ch>
2010-10-22 20:26:23 +0000

    oups, I forgot the new library header to be included by the applications using libsnowpack...

---
+++ snowpack/libsnowpack.h
@@ -0,0 +1,55 @@
+/*
+ *  SNOWPACK VERSION 9.x
+ *
+ *  Copyright WSL Institute for Snow and Avalanche Research SLF, DAVOS, SWITZERLAND
+*/
+/*  This file is part of libsnowpack.
+    libsnowpack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libsnowpack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libsnowpack.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file libsnowpack.h
+ * @version 10.02
+ * This is the header file to include for projects making use of libsnowpack
+ */
+
+#ifndef __LIBSNOWPACK_H__
+#define __LIBSNOWPACK_H__
+
+
+#include <snowpack/Aggregate.h>
+#include <snowpack/AsciiIO.h>
+#include <snowpack/Canopy.h>
+#include <snowpack/Constants.h>
+#include <snowpack/DataClasses.h>
+#include <snowpack/Hazard.h>
+//#include <snowpack/ImisDBIO.h>
+#include <snowpack/Laws.h>
+#include <snowpack/Laws_sn.h>
+#include <snowpack/Metamorphism.h>
+#include <snowpack/Meteo.h>
+#include <snowpack/PhaseChange.h>
+#include <snowpack/Radiation.h>
+#include <snowpack/Saltation.h>
+#include <snowpack/SnowDrift.h>
+#include <snowpack/SnowpackConfig.h>
+#include <snowpack/Snowpack.h>
+#include <snowpack/SnowpackIO.h>
+#include <snowpack/SnowpackIOInterface.h>
+#include <snowpack/Solver.h>
+#include <snowpack/Stability.h>
+#include <snowpack/Utils.h>
+#include <snowpack/WaterTransport.h>
+
+#endif
+
