f79fc00492f09139eef51ff510e08c9fda100ebb
Nander Wever <Nander.Wever@colorado.edu>
2012-07-31 08:26:21 +0000

    The mass balance test should work now. Script returns ERROR when test fails (either met file is not readable, not found, or sum of mass balance error is larger than 10 kg/m^2), else 0.

--- tests/massbalance/run_test.sh
+++ tests/massbalance/run_test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 TESTDELTA=10
-
-bash massbalancecheck.sh ../res1exp/output/MST96_res.met 2>&1 >/dev/null | sed -r 's/*(ERROR)*/\1/' | awk '(NR==1) {print ($NF>'${TESTDELTA}' || NF==1)?"ERROR":0}'
+bash massbalancecheck.sh ../res1exp/output/MST96_res.met 2>&1 >/dev/null | sed -r 's/.*(ERROR).*/\1/' | awk '(NR==1) {print ($NF>'${TESTDELTA}' || NF==1)?"ERROR":0}'
