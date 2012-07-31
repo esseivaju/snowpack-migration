9af1f841a94434ed23d1cd66ab7f026f8eddcd1d
David Zanella <>
2012-07-31 23:12:27 +0000

    - Add Coverage Build Type for testing - make simpler res1exp test start script (this time all files should be commited ...)

--- tests/res1exp/run_res1exp.sh
+++ tests/res1exp/run_res1exp.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# use  nohup *.sh > fout  to redirect output to named file
-
-# do valgrind with ctest !!!!
-#TOOL="valgrind --tool=callgrind --simulate-cache=yes"
-#TOOL="/software/bin/valgrind --tool=memcheck --leak-check=full --show-reachable=yes --track-origins=yes --log-fd=2 "
-#TOOL="time"
-#TOOL=""
-#${TOOL} ../../bin/snowpack -c input/io_res1exp.ini -e 1996-06-17T00:00
-
 ../../bin/snowpack -c input/io_res1exp.ini -e 1996-06-17T00:00
