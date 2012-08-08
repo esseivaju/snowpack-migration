f2b91b8ac6c243a9ff0905e79bb5342e1f4e97b1
Mathias Bavay <bavay@slf.ch>
2012-08-08 09:55:19 +0000

    Adding test of Doorschot saltation in the res1exp test (for better coverage). Some cleanup in the test files.

--- tests/res1exp/run_res1exp.sh
+++ tests/res1exp/run_res1exp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-../../bin/snowpack -c input/io_res1exp.ini -e 1996-06-17T00:00
+../../bin/snowpack -c io_res1exp.ini -e 1996-06-17T00:00
+../../bin/snowpack -c io_doorschot.ini -e 1996-06-17T00:00
