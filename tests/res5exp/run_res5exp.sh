3ce8c2cc165ab5bcc69801ed18481a622731dec5
Mathias Bavay <bavay@slf.ch>
2014-01-13 14:54:50 +0000

    Finally, the 5 expositions test works properly and has a sane structure. The necessary build code has been added for CAAMLIO.

--- tests/res5exp/run_res5exp.sh
+++ tests/res5exp/run_res5exp.sh
@@ -3,36 +3,36 @@
 
 PREC="1e-3"
 #north slopes
-numdiff -r ${PREC} output_ref/MST961_tst.haz output/MST961_tst.haz | grep "+++"
-numdiff -r ${PREC} output_ref/MST961_tst.sno output/MST961_tst.sno | grep "+++"
+numdiff -r ${PREC} output_ref/MST961_res4.haz output/MST961_res4.haz | grep "+++"
+#numdiff -r ${PREC} output_ref/MST961_res4.sno output/MST961_res4.sno | grep "+++"
 
 #south slopes
-numdiff -r ${PREC} output_ref/MST963_tst.haz output/MST963_tst.haz | grep "+++"
-numdiff -r ${PREC} output_ref/MST963_tst.sno output/MST963_tst.sno | grep "+++"
+numdiff -r ${PREC} output_ref/MST963_res4.haz output/MST963_res4.haz | grep "+++"
+#numdiff -r ${PREC} output_ref/MST963_res4.sno output/MST963_res4.sno | grep "+++"
 
 #north slopes
-rm -f output_ref/MST961_tst.met
-bunzip2 -k output_ref/MST961_tst.met.bz2
-sed -i '11d' output_ref/MST961_tst.met; sed -i '11d' output/MST961_tst.met
-numdiff -s', \t\n' -r ${PREC} --speed-large-files output_ref/MST961_tst.met output/MST961_tst.met | grep "+++"
-rm -f output_ref/MST961_tst.met
+rm -f output_ref/MST961_res4.met
+bunzip2 -k output_ref/MST961_res4.met.bz2
+sed -i '11d' output_ref/MST961_res4.met; sed -i '11d' output/MST961_res4.met
+numdiff -s', \t\n' -r ${PREC} --speed-large-files output_ref/MST961_res4.met output/MST961_res4.met | grep "+++"
+rm -f output_ref/MST961_res4.met
 
-rm -f output_ref/MST961_tst.pro
-bunzip2 -k output_ref/MST961_tst.pro.bz2
-sed -i '10d' output_ref/MST961_tst.pro; sed -i '10d' output/MST961_tst.pro
-numdiff -s', \t\n' -r ${PREC} --speed-large-files output_ref/MST961_tst.pro output/MST961_tst.pro | grep "+++"
-rm -f output_ref/MST961_tst.pro
+rm -f output_ref/MST961_res4.pro
+bunzip2 -k output_ref/MST961_res4.pro.bz2
+sed -i '10d' output_ref/MST961_res4.pro; sed -i '10d' output/MST961_res4.pro
+numdiff -s', \t\n' -r ${PREC} --speed-large-files output_ref/MST961_res4.pro output/MST961_res4.pro | grep "+++"
+rm -f output_ref/MST961_res4.pro
 
 #south slopes
-rm -f output_ref/MST963_tst.met
-bunzip2 -k output_ref/MST963_tst.met.bz2
-sed -i '11d' output_ref/MST963_tst.met; sed -i '11d' output/MST963_tst.met
-numdiff -s', \t\n' -r ${PREC} --speed-large-files output_ref/MST963_tst.met output/MST963_tst.met | grep "+++"
-rm -f output_ref/MST963_tst.met
+rm -f output_ref/MST963_res4.met
+bunzip2 -k output_ref/MST963_res4.met.bz2
+sed -i '11d' output_ref/MST963_res4.met; sed -i '11d' output/MST963_res4.met
+numdiff -s', \t\n' -r ${PREC} --speed-large-files output_ref/MST963_res4.met output/MST963_res4.met | grep "+++"
+rm -f output_ref/MST963_res4.met
 
-rm -f output_ref/MST963_tst.pro
-bunzip2 -k output_ref/MST963_tst.pro.bz2
-sed -i '10d' output_ref/MST963_tst.pro; sed -i '10d' output/MST963_tst.pro
-numdiff -s', \t\n' -r ${PREC} --speed-large-files output_ref/MST963_tst.pro output/MST963_tst.pro | grep "+++"
-rm -f output_ref/MST963_tst.pro
+rm -f output_ref/MST963_res4.pro
+bunzip2 -k output_ref/MST963_res4.pro.bz2
+sed -i '10d' output_ref/MST963_res4.pro; sed -i '10d' output/MST963_res4.pro
+numdiff -s', \t\n' -r ${PREC} --speed-large-files output_ref/MST963_res4.pro output/MST963_res4.pro | grep "+++"
+rm -f output_ref/MST963_res4.pro
 
