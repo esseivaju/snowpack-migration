41872edfdd47f736f73a69539fcd0bea1211778d
Mathias Bavay <bavay@slf.ch>
2014-04-25 14:49:38 +0000

    One of the backported commit was making trouble: "WaterTransport.cc:419: void WaterTransport::compSurfaceSublimation(const CurrentMeteo&, double, SnowStation&, SurfaceFluxes&): Assertion `EMS[e].Rho>=0. || EMS[e].Rho==IOUtils::nodata' failed." It has been commented out until a fix is implemented (ie reverting this change back to what it was before).

--- snowpack/WaterTransport.cc
+++ snowpack/WaterTransport.cc
@@ -564,11 +564,11 @@ void WaterTransport::mergingElements(SnowStation& Xdata, SurfaceFluxes& Sdata)
 		} else {
 			enforce_merge = false;
 		}
-		const double theta_r=((iwatertransportmodel_snow==RICHARDSEQUATION && eUpper>=Xdata.SoilNode) || (iwatertransportmodel_soil==RICHARDSEQUATION && eUpper<Xdata.SoilNode)) ? (PhaseChange::RE_theta_r) : (PhaseChange::theta_r);
+		//const double theta_r=((iwatertransportmodel_snow==RICHARDSEQUATION && eUpper>=Xdata.SoilNode) || (iwatertransportmodel_soil==RICHARDSEQUATION && eUpper<Xdata.SoilNode)) ? (PhaseChange::RE_theta_r) : (PhaseChange::theta_r);
 		if (((EMS[eUpper].theta[ICE] < Snowpack::min_ice_content) || enforce_merge)
 		       && (EMS[eUpper].theta[SOIL] < Constants::eps2)
-		           && (EMS[eUpper].mk % 100 != 9)  	// no PLASTIC or WATER_LAYER please
-			       && !(eUpper > 0 && eUpper == nE-1 && EMS[eUpper].L > Constants::eps && EMS[eUpper-1].theta[SOIL] < Constants::eps && EMS[eUpper].theta[ICE] > Constants::eps && EMS[eUpper].theta[WATER] < theta_r + Constants::eps && EMS[eUpper-1].theta[WATER] > theta_r + Constants::eps)) {	// Don't merge a dry surface snow layer with a wet one below, as the surface node may then experience a sudden increase in temperature, destroying energy balance.
+		           && (EMS[eUpper].mk % 100 != 9) ){  	// no PLASTIC or WATER_LAYER please
+			       //&& !(eUpper > 0 && eUpper == nE-1 && EMS[eUpper].L > Constants::eps && EMS[eUpper-1].theta[SOIL] < Constants::eps && EMS[eUpper].theta[ICE] > Constants::eps && EMS[eUpper].theta[WATER] < theta_r + Constants::eps && EMS[eUpper-1].theta[WATER] > theta_r + Constants::eps)) {	// Don't merge a dry surface snow layer with a wet one below, as the surface node may then experience a sudden increase in temperature, destroying energy balance.
 			bool UpperJoin=false;			// Default is joining with elements below
 			bool merged = true;		// true: element is finally merged, false: element is finally removed.
 			if (eUpper > Xdata.SoilNode) { 		// If we have snow elements below to merge with
